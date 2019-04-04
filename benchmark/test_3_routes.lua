12f451f5da2e8996344872f1bdf4912347b0708f
Yuansheng Wang <membphis@gmail.com>
2019-04-04 15:29:48 +0800

    change: added step output for benchmark script.

--- benchmark/test_3_routes.lua
+++ benchmark/test_3_routes.lua
@@ -7,6 +7,7 @@ function time(title, block)
   local ed = os.clock()
 
   ngx.say(title .. ": " .. ed-st.. " sec")
+  ngx.flush()
 end
 
 -- handler
@@ -23,18 +24,27 @@ r:get("/", foo)
 for i = 1, 1000 do
     r:get("/a/" .. i, foo)
 end
+ngx.say("insert routes 1th")
 
 r:get("/foo/bar/baz/hoge/fuga/piyo", foo)
 for i = 1, 1000 do
     r:get("/foo/bar/baz/hoge/fuga/piyo/" .. i, foo)
 end
+ngx.say("insert routes 2th")
 
 r:insert_route({"GET", "POST"}, "/foo/{id}/{name}", foo)
 for i = 1, 1000 do
     r:insert_route({"GET", "POST"}, "/bar/{id}/{name}" .. i, foo)
 end
+ngx.say("insert route3 3th")
+
+ngx.say("inserted all routes")
+ngx.flush()
 
 r:compile()
+
+ngx.say("compiled routes")
+ngx.flush()
 
 ----------------------------------------------------------------------
 -- bench 1
@@ -56,4 +66,19 @@ time("get /foo/{id}/{name}", function()
   for i=0, 10000000 do
       r:dispatch("GET", "/foo/123/999")
   end
+end)
+
+-- bench 4
+time("fetch from lua table by key", function()
+    local t = {}
+    for i=1,1000 do
+        t["a" .. i] = i
+    end
+
+    local v
+    for i=0, 10000000 do
+        v = t["a3"]
+    end
+
+    ngx.say("fetched val: ", v)
 end)
